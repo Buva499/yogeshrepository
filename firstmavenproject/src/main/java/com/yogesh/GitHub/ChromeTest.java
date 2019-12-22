1ad729d7c8afc69682111443fc709777c07678ef
Yogesh <Yogesh@LAPTOP-3NQHFAH7>
2019-12-22 17:43:20 -0300

    commit1

---
+++ firstmavenproject/src/main/java/com/yogesh/GitHub/ChromeTest.java
@@ -0,0 +1,20 @@
+package com.yogesh.GitHub;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeTest {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://magento.com/");
+		Thread.sleep(3000);
+		driver.quit();
+	}
+
+}
